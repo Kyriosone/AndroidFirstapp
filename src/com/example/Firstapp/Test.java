b5d24c23d3ddb57a0d91d39a0b2737288147b96a
Kyriosone <kyrios@inbox.ru>
2013-10-22 19:49:00 +0400

    testing git+IDEA work

---
+++ src/com/example/Firstapp/Test.java
@@ -0,0 +1,11 @@
+package com.example.Firstapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 1
+ * Date: 22.10.13
+ * Time: 19:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
